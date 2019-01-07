df3a54352dcc1464a9e6721c0772fc1289ef6190
Venky Krishnaswamy <Venky.Krishnaswamy@panerabread.com>
2019-01-06 21:59:52 -0600

    Removed dummy statements

--- src/app.js
+++ src/app.js
@@ -12,12 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
-store.dispatch(addExpense( {description: 'Water Bill', amount: 4500 } ));
-store.dispatch(addExpense( {description: 'Gas Bill', createdAt: 1000 } ));
-store.dispatch(addExpense( {description: 'Rent', amount: 109500, createdAt: 2000} ));
-
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
