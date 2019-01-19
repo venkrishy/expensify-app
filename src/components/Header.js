1b8747d40d6c84463338e9dd9410009c2612dc7a
Venky Krishnaswamy <Venky.Krishnaswamy@panerabread.com>
2019-01-19 11:09:35 -0600

    Created public and private routs

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ( {startLogout} ) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         -  <NavLink to="/create" activeClassName="is-active">Create</NavLink>
         - <NavLink to="/edit" activeClassName="is-active">Edit</NavLink> 
         - <NavLink to="/help" activeClassName="is-active">Help</NavLink>
